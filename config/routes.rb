2c63bb2f835860c27111da6ea11ddfa6b1c769f3
Victoria Chiang <vchiang18@gmail.com>
2014-02-26 03:56:03 +0000

    Changed home_page path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Demoapp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root 'static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -14,7 +14,6 @@ Demoapp::Application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
